mrutkows/strong-registry

commit 5db99864c8bd8addc921c9854a7f4551c950fd31

Extract RegistryConfig hasAuthCredentials
Move the code determining whether the registry config has auth
credentials and whether it requires `npm login` to a standalone file
to allow reuse.

lib/commands/use.js
@@ -11,7 +11,7 @@ module.exports = function useRegistry($0, name) {
   }
 
   var rc = loadRegistryConfigOrExit(name);
-  var npmrc = storage.tryReadIniFile(NPM_RC_PATH, {});
+  var npmrc = new RegistryConfig(storage.tryReadIniFile(NPM_RC_PATH, {}));
 
   updateCurrentRegistryFromNpmRc(npmrc);
 
@@ -20,7 +20,7 @@ module.exports = function useRegistry($0, name) {
 
   console.log('Using the registry "%s" (%s).', name, rc.registry);
 
-  if (npmrc['always-auth'] && !hasAuth(npmrc)) {
+  if (npmrc.requiresLogin()) {
     console.log('The registry requires authentication for all requests.');
     console.log('Running `npm login` to setup credentials.');
     spawnNpmLogin();
@@ -83,8 +83,4 @@ function wasUpdated(old, current) {
     });
 
   return !!diffs.length;
-}
-
-function hasAuth(config) {
-  return config._auth || (config.username && config._password);
 }

lib/registry-config.js
@@ -73,3 +73,11 @@ function copyTrackedOptions(src, dst, excluding) {
       dst[k] = src[k];
   });
 }
+
+RegistryConfig.prototype.requiresLogin = function() {
+  return this['always-auth'] && !this.hasAuthCredentials();
+};
+
+RegistryConfig.prototype.hasAuthCredentials = function() {
+  return this._auth || (this.username && this._password);
+};

commit 476238f9af3041edc305f2b85764a1c2b69b4826

Preserve non-registry options in ~/.npmrc
The bug was introduced by 5db99864.

lib/commands/use.js
@@ -11,7 +11,7 @@ module.exports = function useRegistry($0, name) {
   }
 
   var rc = new RegistryConfig(storage.loadOrExit(name));
-  var npmrc = new RegistryConfig(storage.tryReadIniFile(NPM_RC_PATH, {}));
+  var npmrc = storage.tryReadIniFile(NPM_RC_PATH, {});
 
   updateCurrentRegistryFromNpmRc(npmrc);
 
@@ -20,7 +20,7 @@ module.exports = function useRegistry($0, name) {
 
   console.log('Using the registry "%s" (%s).', name, rc.registry);
 
-  if (npmrc.requiresLogin()) {
+  if (new RegistryConfig(npmrc).requiresLogin()) {
     console.log('The registry requires authentication for all requests.');
     console.log('Running `npm login` to setup credentials.');
     spawnNpmLogin();

test/cli-use.test.js
@@ -46,6 +46,17 @@ describe('`sl-registry use`', function() {
       });
   });
 
+  it('preserves entries not related to registry config', function() {
+    sandbox.givenAdditionalEntry('custom');
+    sandbox.givenUserNpmRc({ browser: 'firefox' });
+    return new CliRunner(['use', 'custom'])
+      .run()
+      .then(function() {
+        var npmrc = sandbox.readUserNpmRrc();
+        expect(npmrc.browser).to.equal('firefox');
+      });
+  });
+
   it('updates registry config from ~/.npmrc', function() {
     sandbox.givenAdditionalEntry('custom');
     sandbox.givenUserNpmRc({ _auth: 'user:name' });